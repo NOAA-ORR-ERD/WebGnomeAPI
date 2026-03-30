11cb3b4f7650affa29f298f4efa1f61e55f33ce6
Chander Ganesan <chander.ganesan@noaa.gov>
2026-03-30 12:03:08 -0400

    source activate in our entrypoint so that we always have a proper env setup

--- config_files/docker_entrypoint.sh
+++ config_files/docker_entrypoint.sh
@@ -2,5 +2,5 @@
 #
 # Handle the startup using the environment variable that sets up the 
 # deployment scenario
-
+source /activate.sh
 exec supervisord --configuration /config_files/supervisord.conf -n
