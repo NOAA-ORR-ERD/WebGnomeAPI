998d1685e96b8597a10db40645d602ea80bd9ca2
Naomi Connolly <naomi.connolly@noaa.gov>
2026-04-17 12:19:10 -0700

    updated paths for purge scripts

--- scripts/aws-purge-old-forcasts.sh
+++ scripts/aws-purge-old-forcasts.sh
@@ -9,7 +9,7 @@ else
 	find=$(which find)
 fi
 
-FORECAST_SIZE=$($find /data/libgoods-cache -regextype posix-extended -regex '.*.f[0-9]{3}.*' -exec stat -c "%s" {} + | awk '{s+=$1} END {print s}')
+FORECAST_SIZE=$($find /archive -regextype posix-extended -regex '.*.f[0-9]{3}.*' -exec stat -c "%s" {} + | awk '{s+=$1} END {print s}')
 if [[ "$FORECAST_SIZE" ]]; then
   echo 'FORECAST SIZE BEFORE:' $(($FORECAST_SIZE / 1024 / 1024 / 1024))'gb'
 else
@@ -17,9 +17,9 @@ else
 fi
 
 # remove forecast files older than a day
-$find /data/libgoods-cache -regextype posix-extended -regex '.*.f[0-9]{3}.*' -mmin +$OLDERTHAN -exec rm {} \;
+$find /archive -regextype posix-extended -regex '.*.f[0-9]{3}.*' -mmin +$OLDERTHAN -exec rm {} \;
 
-FORECAST_SIZE=$($find /data/libgoods-cache -regextype posix-extended -regex '.*.f[0-9]{3}.*' -exec stat -c "%s" {} + | awk '{s+=$1} END {print s}')
+FORECAST_SIZE=$($find /archive -regextype posix-extended -regex '.*.f[0-9]{3}.*' -exec stat -c "%s" {} + | awk '{s+=$1} END {print s}')
 if [[ "$FORECAST_SIZE" ]]; then
   echo 'FORECAST TOTAL SIZE AFTER CLEANUP:' $(($FORECAST_SIZE / 1024 / 1024 / 1024))'gb'
 else
